161b129c757349739773c3d8350dc3687a606719
Dave Calnan <dave@jiff.ie>
2018-03-02 18:53:17 +0000

    Fix anonymous comment issue

--- public/forms/comment.php
+++ public/forms/comment.php
@@ -4,7 +4,7 @@ require '../../src/helpers.php';
 require '../../src/database/connection.php';
 
 $post_id = $_POST['post_id'];
-$user_id = isset($_POST['user_id']) ? $_POST['user_id'] : null;
+$user_id = isset($_POST['user_id']) ? $_POST['user_id'] : 0;
 $name = htmlspecialchars($_POST['name'], ENT_QUOTES);
 $email = htmlspecialchars($_POST['email'], ENT_QUOTES);
 $body = htmlspecialchars($_POST['body'], ENT_QUOTES);
