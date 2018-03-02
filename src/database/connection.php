3dc05e705dd1576a39796f90a36c0c5eb4af6ee8
Dave Calnan <dave@jiff.ie>
2018-03-02 18:04:44 +0000

    Added mysql debugging

--- src/database/connection.php
+++ src/database/connection.php
@@ -8,6 +8,7 @@ $user = 'is4444';
 $password = 'is4444';
 $database = 'is4444';
 
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $mysqli = new mysqli($host, $user, $password, $database);
 
 function validateLogin($email, $password) {
